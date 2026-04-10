62c39d8112d47e4175f2e1e01bff7f78901187cb
DingYong <2903964879@qq.com>
2026-04-11 02:10:01 +0800

    添加PID控制模块的增益设置、积分和微分更新功能

--- UserLib/Module/Algorithm/PID/PID.cpp
+++ UserLib/Module/Algorithm/PID/PID.cpp
@@ -6,3 +6,23 @@
 
 #include "PID.hpp"
 
+void PID::setGains(const float P, const float I, const float D) {
+    _gain_proportional = P;
+    _gain_integral = I;
+    _gain_derivative = D;
+}
+
+// @todo:add cconstrain
+void PID::updateIntegral(float error, const float dt) {
+    const float integral_new = _integral + _gain_integral * error * dt;
+}
+
+void PID::updateDerivative(float feedback, const float dt) {
+    float feedback_change = 0.0f;
+
+    if ((dt > FLT_EPSILON) && std::isfinite(_last_feedback)) {
+        feedback_change = (feedback - _last_feedback) / dt;
+    }
+
+    return feedback_change;
+}
--- UserLib/Module/Algorithm/PID/PID.hpp
+++ UserLib/Module/Algorithm/PID/PID.hpp
@@ -9,5 +9,27 @@
 
 class PID
 {
-
+public:
+    // 默认构造
+    PID() = default;
+    virtual ~PID() = default;
+    // 限幅
+    void setOutputLimit(const float limit) {_limit_integral = limit;}
+    void setIntegralLimit(const float limit){_limit_integral = limit;}
+    // PID 参数 + 目标值
+    void setGains(const float P, const float I, const float D);
+    void setSetpoint(const float setpoint){_setpoint = setpoint;}
+    // 核心计算函数
+    float update(const float feedback, const float dt, const bool update_integral = true);
+    // 积分/微分 重置
+    float getIntegral() { return _integral; }
+    void resetIntegral() { _integral = 0.0f; }
+private:
+    void
+    // Gains,Limits
+    float _gain_proportional = 0.0f;
+    float _gain_integral     = 0.0f;
+    float _gain_derivative   = 0.0f;
+    float _limit_integral    = 0.0f;
+    float _limit_output      = 0.0f;
 };
