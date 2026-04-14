5920e01fab13b5b67b8548629ca58346868cb5a8
dingyong <2903964879@qq.com>
2026-04-14 13:47:44 +0800

    add:constrain约束函数

---
+++ UserLib/lib/mathlib/Functions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by DingYong on 2026/4/14.
+//
+
+#include "Functions.h"
---
+++ UserLib/lib/mathlib/Functions.h
@@ -0,0 +1,12 @@
+//
+// Created by DingYong on 2026/4/14.
+//
+
+#ifndef FLIGHT_Y_FUNCTIONS_H
+#define FLIGHT_Y_FUNCTIONS_H
+
+float constrain(float val ,float min_val,float max_val) {
+    return (val < min_val) ? min_val : ((val > max_val) ? max_val : val);
+}
+
+#endif //FLIGHT_Y_FUNCTIONS_H
