cb807122e5443a9fd76b6a3c138716d83dd61b61
dingyong <2903964879@qq.com>
2026-04-23 02:34:00 +0800

    add the low pass filiter

--- UserLib/Task/IMUupdata.cpp
+++ UserLib/Task/IMUupdata.cpp
@@ -5,33 +5,43 @@
 #include "IMUupdata.hpp"
 
 #include "MPU6050.hpp"
-#include "MahonyAHRS.hpp"
 #include "cmsis_os.h"
 #include "i2c.h"
 
 Mahony mahony;
 static MPU6050 mpu(&hi2c1);
 
-static float gx,gy,gz,ax,ay,az;
-static float roll,pitch,yaw;
+#define k 0.95f
+
+static float acc[3],gyro[3];
+static float euler[3];
+static float euler_last[3];
 void imuUpdate(void const * argument)
 {
     mpu.init();
     for (;;)
     {
         mpu.readData();
-        gx = mpu.data.gyro[0];
-        gy = mpu.data.gyro[1];
-        gz = mpu.data.gyro[2];
-        ax = mpu.data.acc[0];
-        ay = mpu.data.acc[1];
-        az = mpu.data.acc[2];
+        gyro[0] = mpu.data.gyro[0] * DEG_TO_RAD;
+        gyro[1] = mpu.data.gyro[1] * DEG_TO_RAD;
+        gyro[2] = mpu.data.gyro[2] * DEG_TO_RAD;
+        acc[0] = mpu.data.acc[0];
+        acc[1] = mpu.data.acc[1];
+        acc[2] = mpu.data.acc[2];
 
-        mahony.updateIMU(gx,gy,gz,ax,ay,az);
+        mahony.update(gyro[0],gyro[1],gyro[2],acc[0],acc[1],acc[2],0,0,0);
 
-        roll = mahony.getRollRadians();
-        pitch = mahony.getPitchRadians();
-        yaw = mahony.getYawRadians();
+        euler[0] = mahony.getRollRadians();
+        euler[1] = mahony.getPitchRadians();
+        euler[2] = mahony.getYawRadians();
+
+        euler[0] = lowPassFiliter(euler[0],euler_last[0],k);
+        euler[1] = lowPassFiliter(euler[1],euler_last[1],k);
+        euler[2] = lowPassFiliter(euler[2],euler_last[2],k);
+
+        euler_last[0] = euler[0];
+        euler_last[1] = euler[1];
+        euler_last[2] = euler[2];
 
         osDelay(1);
     }
--- UserLib/Task/IMUupdata.hpp
+++ UserLib/Task/IMUupdata.hpp
@@ -4,10 +4,17 @@
 
 #ifndef FLIGHT_Y_IMUUPDAT_HPP
 #define FLIGHT_Y_IMUUPDAT_HPP
+#include "MahonyAHRS.hpp"
+
+extern Mahony mahony;
 
 extern "C" {
-void imuUpdate(void const * argument);
 
+void imuUpdate(void const * argument);
+inline float lowPassFiliter(float &current,float &last,float k)
+{
+    return ((1-k) * current + k * last);
+}
 }
 
 #endif //FLIGHT_Y_IMUUPDAT_HPP
