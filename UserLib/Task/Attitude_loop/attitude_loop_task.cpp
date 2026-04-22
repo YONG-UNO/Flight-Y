f57c42128e253c258c9e787b7f1c2375d915a8b2
dingyong <2903964879@qq.com>
2026-04-23 00:51:43 +0800

    add attitude_loop_task

---
+++ UserLib/Task/Attitude_loop/attitude_loop_task.cpp
@@ -0,0 +1,5 @@
+//
+// Created by DingYong on 2026/4/23.
+//
+
+#include "attitude_loop_task.hpp"
---
+++ UserLib/Task/Attitude_loop/attitude_loop_task.hpp
@@ -0,0 +1,11 @@
+//
+// Created by DingYong on 2026/4/23.
+//
+
+#pragma once
+
+extern "C" {
+
+void attitude_loop_task(void const * argument);
+
+}
